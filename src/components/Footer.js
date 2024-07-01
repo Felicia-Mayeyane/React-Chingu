5a22043e01757340a5ab5ea6961bda969ff2228c
The Data Fairy <119903285+Felicia-Mayeyane@users.noreply.github.com>
2024-07-01 12:38:46 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,17 +4,17 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="social">
-        <a href="#"><i className='bx bxl-instagram-alt'></i></a>
-        <a href="#"><i className='bx bxl-pinterest'></i></a>
-        <a href="#"><i className='bx bxl-youtube'></i></a>
-        <a href="#"><i className='bx bxl-linkedin-square'></i></a>
+        <a href="#insta"><i className='bx bxl-instagram-alt'></i></a>
+        <a href="#pint"><i className='bx bxl-pinterest'></i></a>
+        <a href="#youtube"><i className='bx bxl-youtube'></i></a>
+        <a href="#linkedin"><i className='bx bxl-linkedin-square'></i></a>
       </div>
       <ul className="list">
         <li><a href="#">FAQ</a></li>
-        <li><a href="#">Products</a></li>
-        <li><a href="#">About Us</a></li>
-        <li><a href="#">Contact</a></li>
-        <li><a href="#">Privacy</a></li>
+        <li><a href="#coffee">Products</a></li>
+        <li><a href="#about">About Us</a></li>
+        <li><a href="#contact">Contact</a></li>
+        <li><a href="#homee">Privacy</a></li>
       </ul>
       <p className="copyright">Chingu & Coffee Copyright: Felicia Mayeyane</p>
     </footer>
