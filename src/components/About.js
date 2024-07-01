a0cc4b651ec4e374251745d1ebc55b2e95a36857
The Data Fairy <119903285+Felicia-Mayeyane@users.noreply.github.com>
2024-07-01 12:36:08 +0200

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ function About() {
       <div className="about-content">
         <h2 className="heading">About<span>Us</span></h2>
         <p>In addition to our expertly brewed coffee, Chingu & Coffee offers a tantalizing array of Korean-inspired treats...</p>
-        <a href="#" className="btn">Read More</a>
+        <a href="#about" className="btn">Read More</a>
       </div>
     </section>
   );
