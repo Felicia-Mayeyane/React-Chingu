5730c75b1ff18d8d58762a4063886a15d2edb113
The Data Fairy <119903285+Felicia-Mayeyane@users.noreply.github.com>
2024-07-01 13:13:26 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,13 +9,7 @@ function Footer() {
         <a href="#youtube"><i className='bx bxl-youtube'></i></a>
         <a href="#linkedin"><i className='bx bxl-linkedin-square'></i></a>
       </div>
-      <ul className="list">
-        <li><a href="#">FAQ</a></li>
-        <li><a href="#coffee">Products</a></li>
-        <li><a href="#about">About Us</a></li>
-        <li><a href="#contact">Contact</a></li>
-        <li><a href="#homee">Privacy</a></li>
-      </ul>
+      
       <p className="copyright">Chingu & Coffee Copyright: Felicia Mayeyane</p>
     </footer>
   );
